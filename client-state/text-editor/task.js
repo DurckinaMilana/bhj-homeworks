43fad21ab1eb3620c79106b480a33aa926b0f734
Milana <durckina.milana2016@yandex.ru>
2023-02-01 17:03:50 +0300

    new commit

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,14 @@
+const editor = document.getElementById('editor');
+const clear = document.getElementById('clear');
+
+window.addEventListener('unload', () => {
+    localStorage.setItem('message', editor.value);
+})
+
+window.addEventListener('load', () => {
+    editor.value = localStorage.getItem('message');
+})
+
+clear.addEventListener('click', () => {
+    editor.value = '';
+})
